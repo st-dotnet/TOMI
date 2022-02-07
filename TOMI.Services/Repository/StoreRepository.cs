b88fcd6a86bb706e1191dd6f58159b20eec57c49
stdev <stdev.net@gmail.com>
2022-02-07 17:57:25 +0530

    Commit the code for excel export to database.

--- TOMI.Services/Repository/StoreRepository.cs
+++ TOMI.Services/Repository/StoreRepository.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
 using System.Diagnostics;
 using System.Globalization;
 using System.IO;
@@ -9,8 +11,12 @@ using System.Threading.Tasks;
 using AutoMapper;
 using CsvHelper;
 using CsvHelper.Configuration;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using OfficeOpenXml;
 using TOMI.Data.Database;
 using TOMI.Data.Database.Entities;
 using TOMI.Services.Interfaces;
@@ -23,11 +29,15 @@ namespace TOMI.Services.Repository
         private readonly IMapper _mapper;
         private readonly ILogger<StoreRepository> _logger;
         private readonly TOMIDataContext _context;
-        public StoreRepository(ILogger<StoreRepository> logger, TOMIDataContext context, IMapper mapper)
+        private IConfiguration _configuration;
+        private IWebHostEnvironment _environment;
+        public StoreRepository(ILogger<StoreRepository> logger, TOMIDataContext context, IMapper mapper, IConfiguration configuration, IWebHostEnvironment environment)
         {
             _logger = logger;
             _context = context;
             _mapper = mapper;
+            _configuration = configuration;
+            _environment = environment;
         }
         public async Task<StoreModelResponse> CreateStore(StoreModel store)
         {
@@ -531,160 +541,50 @@ namespace TOMI.Services.Repository
         public async Task<FileUplaodRespone> ParametersByDepartmentData(FilterDataModel model)
         {
             bool isSaveSuccess = false;
-            string fileName;
-
             List<ParametersByDepartment> parametersbydepartmentList = new();
-
             var stopwatch = new Stopwatch();
             stopwatch.Start();
             double timeElapsed = 0;
-
             try
             {
-                var extension = "." + model.File.FileName.Split('.')[model.File.FileName.Split('.').Length - 1];
-                fileName = DateTime.Now.Ticks + extension; //Create a new Name for the file due to security reasons.
-
-                var pathBuilt = Path.Combine(Directory.GetCurrentDirectory(), "Upload\\files");
-
-                if (!Directory.Exists(pathBuilt))
-                {
-                    Directory.CreateDirectory(pathBuilt);
-                }
-
-                var path = Path.Combine(Directory.GetCurrentDirectory(), "Upload\\files",
-                 fileName);
-
-
-                using (var stream = new FileStream(path, FileMode.Create))
+                using (var stream = new MemoryStream())
                 {
                     await model.File.CopyToAsync(stream);
-                }
-                var parametersbydepartmentFile = File.ReadAllLines(path);
-
-                parametersbydepartmentList = parametersbydepartmentFile.SelectMany(x => new List<ParametersByDepartment>
-                {
-                   new()
+                    using (var package = new ExcelPackage(stream))
                     {
-                        Department=(x.Substring(0,4)),
-                        Quantity=(x.Substring(4,4)),
-                        Pesos=(x.Substring(8,6)),
-                        PercentageInPieces=(x.Substring(14,2)),
-                        CustomerId = model.CustomerId,
-                        StoreId = model.StoreId,
-                        StockDate = model.StockDate,
+                        ExcelWorksheet worksheet = package.Workbook.Worksheets["Sheet1"];
+                        var rowcount = worksheet.Dimension.Rows;
+                        var colcount = worksheet.Dimension.Columns;
+                        for (int row = 2; row < rowcount; row++)
+                        {
+                            parametersbydepartmentList.Add(new ParametersByDepartment
+                            {
+                                Department = worksheet.Cells[row, 1].Value.ToString().Trim(),
+                                Quantity = worksheet.Cells[row, 2].Value.ToString().Trim(),
+                                Pesos = worksheet.Cells[row, 3].Value.ToString().Trim(),
+                                PercentageInPieces = worksheet.Cells[row, 4].Value.ToString().Trim(),
+                                CustomerId=model.CustomerId,
+                                StoreId=model.StoreId,
+                                StockDate=model.StockDate
+                            });
+                        }
                     }
-                 }).ToList();
-
-                // Submit the change to the database.
-                try
-                {
-                    await _context.BulkInsertAsync(parametersbydepartmentList);
-                    stopwatch.Stop();
-                    timeElapsed = Math.Ceiling(stopwatch.Elapsed.TotalSeconds);
-
-                    File.Delete(path);
                 }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e);
-
-                }
-                isSaveSuccess = true;
+                //SaveDataToDb(list);
+                _context.ParametersByDepartment.AddRange(parametersbydepartmentList);
+                await _context.SaveChangesAsync();
             }
             catch (Exception e)
             {
                 //log error
             }
-
+            isSaveSuccess = true;
             return new FileUplaodRespone
             {
                 stockRecordCount = parametersbydepartmentList.Count.ToString(),
                 TimeElapsed = timeElapsed,
                 Success = isSaveSuccess
-            }; ; ;
-        }
-
-        public async Task<FileUplaodRespone> OrderJobData(FilterDataModel model)
-        {
-            bool isSaveSuccess = false;
-            string fileName;
-
-            List<OrderJob> orderJobList = new();
-
-            var stopwatch = new Stopwatch();
-            stopwatch.Start();
-            double timeElapsed = 0;
-
-            try
-            {
-                var extension = "." + model.File.FileName.Split('.')[model.File.FileName.Split('.').Length - 1];
-                fileName = DateTime.Now.Ticks + extension; //Create a new Name for the file due to security reasons.
-
-                var pathBuilt = Path.Combine(Directory.GetCurrentDirectory(), "Upload\\files");
-
-                if (!Directory.Exists(pathBuilt))
-                {
-                    Directory.CreateDirectory(pathBuilt);
-                }
-
-                var path = Path.Combine(Directory.GetCurrentDirectory(), "Upload\\files",
-                 fileName);
-
-
-                using (var stream = new FileStream(path, FileMode.Create))
-                {
-                    await model.File.CopyToAsync(stream);
-                }
-                var orderJobFile = File.ReadAllLines(path);
-               
-                string regex = "^0+(?!$)";
-                orderJobList = orderJobFile.Skip(1).SelectMany(x => new List<OrderJob>
-                {
-                   new()
-                    {
-                     
-                        Store = Regex.Replace(x.Substring(0,3),regex, ""),
-                        Code= Regex.Replace(x.Substring(3,14),regex, ""),
-                        Department=Regex.Replace(x.Substring(18,4),regex, ""),
-                        Description =Regex.Replace(x.Substring(22,30),regex, ""),
-                        SalePrice= Regex.Replace(x.Substring(52,8),regex, ""),
-                        PriceWithoutTaxes= Regex.Replace(x.Substring(60,8),regex, ""),
-                        SKU = Regex.Replace(x.Substring(68, 12), regex, ""),
-                        Category=x.Length  ==88 ?x.Substring(82,6):null,
-                        CustomerId = model.CustomerId,
-                        StoreId = model.StoreId,
-                        StockDate = model.StockDate,
-
-                    }
-                 }).ToList();
-
-                // Submit the change to the database.
-                try
-                {
-                    await _context.BulkInsertAsync(orderJobList);
-                    stopwatch.Stop();
-                    timeElapsed = Math.Ceiling(stopwatch.Elapsed.TotalSeconds);
-
-                    File.Delete(path);
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e);
-
-                }
-                isSaveSuccess = true;
-            }
-            catch (Exception e)
-            {
-                //log error
-            }
-
-            return new FileUplaodRespone
-            {
-                stockRecordCount = orderJobList.Count.ToString(),
-                TimeElapsed = timeElapsed,
-                Success = isSaveSuccess
-            }; ; ;
+            };
         }
         public async Task<FileUplaodRespone> StocksData(FilterDataModel stocksData)
         {
@@ -841,6 +741,89 @@ namespace TOMI.Services.Repository
         {
             return await _context.OrderJob.Where(c => c.CustomerId == request.CustomerId && c.StoreId == request.StoreId && c.StockDate.Value.Date == request.StockDate.Value.Date).Take(500).ToListAsync();
 
+        }
+
+        public async Task<FileUplaodRespone> OrderJobData(FilterDataModel model)
+        {
+            bool isSaveSuccess = false;
+            string fileName;
+
+            List<OrderJob> orderJobList = new();
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            double timeElapsed = 0;
+
+            try
+            {
+                var extension = "." + model.File.FileName.Split('.')[model.File.FileName.Split('.').Length - 1];
+                fileName = DateTime.Now.Ticks + extension; //Create a new Name for the file due to security reasons.
+
+                var pathBuilt = Path.Combine(Directory.GetCurrentDirectory(), "Upload\\files");
+
+                if (!Directory.Exists(pathBuilt))
+                {
+                    Directory.CreateDirectory(pathBuilt);
+                }
+
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "Upload\\files",
+                 fileName);
+
+
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    await model.File.CopyToAsync(stream);
+                }
+                var orderJobFile = File.ReadAllLines(path);
+
+                string regex = "^0+(?!$)";
+                orderJobList = orderJobFile.Skip(1).SelectMany(x => new List<OrderJob>
+                {
+                   new()
+                    {
+
+                        Store = Regex.Replace(x.Substring(0,3),regex, ""),
+                        Code= Regex.Replace(x.Substring(3,14),regex, ""),
+                        Department=Regex.Replace(x.Substring(18,4),regex, ""),
+                        Description =Regex.Replace(x.Substring(22,30),regex, ""),
+                        SalePrice= Regex.Replace(x.Substring(52,8),regex, ""),
+                        PriceWithoutTaxes= Regex.Replace(x.Substring(60,8),regex, ""),
+                        SKU = Regex.Replace(x.Substring(68, 12), regex, ""),
+                        Category=x.Length  ==88 ?x.Substring(82,6):null,
+                        CustomerId = model.CustomerId,
+                        StoreId = model.StoreId,
+                        StockDate = model.StockDate,
+
+                    }
+                 }).ToList();
+
+                // Submit the change to the database.
+                try
+                {
+                    await _context.BulkInsertAsync(orderJobList);
+                    stopwatch.Stop();
+                    timeElapsed = Math.Ceiling(stopwatch.Elapsed.TotalSeconds);
+
+                    File.Delete(path);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+
+                }
+                isSaveSuccess = true;
+            }
+            catch (Exception e)
+            {
+                //log error
+            }
+
+            return new FileUplaodRespone
+            {
+                stockRecordCount = orderJobList.Count.ToString(),
+                TimeElapsed = timeElapsed,
+                Success = isSaveSuccess
+            }; ; ;
         }
     }
 }
